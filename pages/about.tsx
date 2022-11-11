b410568092981cd480016afb561a767726940603
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-11 12:55:34 -0300

    messi3

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
-
+import { motion } from 'framer-motion';
 const about = () => {
   return (
     <div>
-        <h1>ABOUT</h1>
+        <motion.h1
+          initial={{ opacity: 0}}
+          animate={{
+              opacity:1,
+              transition: { duration: 4000, type: "spring", },
+          }}
+        >
+          ABOUT
+        </motion.h1>
         <hr />
         <Link href={'/'}>Vamono al home</Link>
     </div>
