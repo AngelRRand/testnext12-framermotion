d45da5f7f9b0b2335a9f825eee77a347b4e3fc3e
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-12 17:36:32 -0300

    config duration

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,17 +6,7 @@ import { motion } from 'framer-motion';
 const about = () => {
   return (
     <PageLayoud title='About' titleColor='crimson'>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { duration: 5, type: "spring", },
-        }}
-        exit={{ opacity: 0, transition: { duration: 1, type: "spring", }, }}
-      >
-
-      <Link href={'/'}>Vamono al home</Link>
-      </motion.div>
+        <Link href={'/'}>Vamono al home</Link>
     </PageLayoud>
   )
 }
