602b8e4bcb1c145c7a474f74a86b553fd5cfe016
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-13 20:44:12 -0300

    break

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ const about: React.FC<Props> = ({ articles }) => {
   return (
     <PageLayoud title='About' titleColor='crimson'>
         <Link href={'/'}>Vamono al home</Link>
-         {articles.length > 0 && articles.map((article:any, index:any) => {
+         {articles.map((article, index) => {
           <div key={index}>
             <h2>{article.title}</h2>
             <p>{article.description}</p>
@@ -21,9 +21,7 @@ export async function getServer(){
   const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d36b270478c045cab6b7cb6fd094c357')
   const { articles } = await response.json()
   return {
-    props: {
-      articles
-    }
+    props: articles
   }
 }
 
